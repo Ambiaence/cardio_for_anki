b2dda0c983321ddb63ff11e27e406d665040eb08
George Kent Allen <main@georgeallen.net>
2024-03-30 11:00:01 -0700

    Multiple Pages Tested. Commit before potential refactor.

--- application/main.go
+++ application/main.go
@@ -17,9 +17,25 @@ import (
 	"gioui.org/unit"
 	"gioui.org/widget"
 	"gioui.org/widget/material"
-
+	"gioui.org/io/key"
+	_"gioui.org/io/system"
 	"georgeallen.net/audio_cards/controller"
 )
+
+type mode struct {
+	value int
+}
+
+func (m *mode) next_mode (){
+	fmt.Println("-----")
+	fmt.Println(m.value)
+	m.value = m.value + 1
+	fmt.Println(m.value)
+
+	if m.value > 2 {
+		m.value = 0
+	}
+}
 
 type (
 	D = layout.Dimensions
@@ -28,6 +44,7 @@ type (
 
 type WordButton struct {
 	text string
+	word string
 	area *widget.Clickable
 	number int	
 }
@@ -56,9 +73,12 @@ var (
 )
 
 var words = []string{"One","two","three"}  
+var sentence string 
+var control_state = mode{value: 0}
 
 func main() {
 	controller.GenerateSpokenWord("The word", "English")
+
 	go func() {
 		w := app.NewWindow(app.Size(unit.Dp(800), unit.Dp(700)))
 		if err := loop(w); err != nil {
@@ -81,6 +101,8 @@ func loop(window *app.Window) error {
 		case app.DestroyEvent:
 			return event.Err
 		case app.FrameEvent:
+			//ev, _ := event.Source.Event(key.Filter{}) 
+			//print(ev)
 			gtx := app.NewContext(&ops, event)
 			dashboard(gtx, theme)
 			event.Frame(gtx.Ops)
@@ -91,16 +113,44 @@ func loop(window *app.Window) error {
 func dashboard(gtx layout.Context, th *material.Theme) layout.Dimensions {
 	for {
 		e, ok := lineEditor.Update(gtx)
+
 		if !ok {
 			break
 		}
+
 		if e, ok := e.(widget.SubmitEvent); ok {
+			sentence = string(e.Text)
 			words = strings.Fields(e.Text)
 			lineEditor.SetText("")
 			for _, word := range words {
 				fmt.Print(word, "\n")
 			}
 			update_words = true;
+		}
+	}
+
+	for {
+		key_event, ok := gtx.Source.Event(key.Filter{})
+		if !ok {
+			break
+		}
+
+		key, ok := key_event.(key.Event)
+		if !ok {
+			continue
+		}
+		
+		if key.Name == "Tab" {
+			control_state.next_mode()
+		}
+
+		switch state := control_state.value; state {
+			case 0: 
+				fmt.Println("One")
+			case 1:
+				fmt.Println("Two")
+			case 2:
+				fmt.Println("Thr")
 		}
 	}
 
@@ -122,16 +172,16 @@ func dashboard(gtx layout.Context, th *material.Theme) layout.Dimensions {
 		buttons_new := func(gtx C, i int) D {
 			var button WordButton
 			text := strconv.Itoa(i) 
-			text += ": "
+			text += " - "
 			text += words[i]
 
+			button.word = words[i] 
 			button.area = new(widget.Clickable)
 			button.text = text
 			button.number = i
+
 			word_buttons = append(word_buttons, button)
 			update_words = false;
-
-
 			return material.Button(th, button.area, button.text).Layout(gtx)
 		}
 
@@ -154,16 +204,28 @@ func dashboard(gtx layout.Context, th *material.Theme) layout.Dimensions {
 		return flex.Layout(gtx, layout.Flexed(1, anon_list))
 	}
 
-	widgets := []layout.Widget{
-		editor,
+	stage_one := []layout.Widget{
 		buttons,
 	}
 
+	stage_two := []layout.Widget{
+		editor,
+	}
+
+	widgets := &stage_one
+
+	if (control_state.value == 1) {
+		widgets = &stage_one
+	} else {
+		widgets = &stage_two
+	}
+
+
 	spaced := func(gtx C, i int) D {
-		return layout.UniformInset(unit.Dp(16)).Layout(gtx, widgets[i])
+		return layout.UniformInset(unit.Dp(16)).Layout(gtx, (*widgets)[i])
 	}
 
 	list_style := material.List(th, list)
-	dimensions := list_style.Layout(gtx, len(widgets), spaced)
+	dimensions := list_style.Layout(gtx, len((*widgets)), spaced)
 	return dimensions
 }
